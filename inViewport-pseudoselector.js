ab97523472eaebb5384a0fceb12cfee2938621d2
Henry Robert <henry@adodis.com>
2017-11-30 13:00:28 +0530

    Create inViewport-pseudoselector.js

---
+++ inViewport-pseudoselector.js
@@ -0,0 +1,30 @@
+/*
+* Find the selector if its in the viewport
+*/
+(function($) {
+  $.belowthefold = function(d, e) { return $(window).height() + $(window).scrollTop() <= $(d).offset().top - e.threshold };
+  $.abovethetop = function(d, e) { return $(window).scrollTop() >= $(d).offset().top + $(d).height() - e.threshold };
+  $.rightofscreen = function(d, e) { return $(window).width() + $(window).scrollLeft() <= $(d).offset().left - e.threshold };
+  $.leftofscreen = function(d, e) { return $(window).scrollLeft() >= $(d).offset().left + $(d).width() - e.threshold };
+  $.inviewport = function(d, e) { return !$.rightofscreen(d, e) && !$.leftofscreen(d,e) && !$.belowthefold(d, e) && !$.abovethetop(d, e) };
+  $.extend($.expr[":"], {
+    "below-the-fold": function(d) {
+      return $.belowthefold(d, { threshold: 0 })
+    },
+    "above-the-top": function(d) {
+      return $.abovethetop(d, { threshold: 0 })
+    },
+    "left-of-screen": function(d) {
+      return $.leftofscreen(d, { threshold: 0 })
+    },
+    "right-of-screen": function(d) {
+      return $.rightofscreen(d, { threshold: 0 })
+    },
+    "in-viewport": function(d) {
+      return $.inviewport(d, { threshold: 0 })
+    }
+  })
+})(jQuery);
+
+/*Example*/
+$("#footer:in-viewport")
